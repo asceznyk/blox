6121ff4449fa23018746517315cad63642e72359
asceznyk <asceznyk@gmail.com>
2021-09-19 15:20:54 +0530

    read file

--- js/blox.js
+++ js/blox.js
@@ -1,3 +1,24 @@
+function run(program) {
+	console.log(program);
+	//let parsed = parse(program)
+}
+
+function readSingleFile(e) {
+  var file = e.target.files[0];
+  if (!file) {
+    return;
+  }
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    var contents = e.target.result;
+    run(contents);
+  };
+  reader.readAsText(file);
+}
+
+document.getElementById('file-input').addEventListener('change', readSingleFile, false);
+
+
 
 
 
