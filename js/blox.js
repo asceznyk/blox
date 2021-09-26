d147f56d4033633740f78b463a6df266b2d0a269
asceznyk <asceznyk@gmail.com>
2021-09-26 14:31:41 +0530

    done with frontend!

--- js/blox.js
+++ js/blox.js
@@ -3,23 +3,6 @@ function run(program) {
 	//let parsed = parse(program)
 }
 
-function readSingleFile(e) {
-  var file = e.target.files[0];
-  if (!file) {
-    return;
-  }
-  var reader = new FileReader();
-  reader.onload = function(e) {
-    var contents = e.target.result;
-    run(contents);
-  };
-  reader.readAsText(file);
-}
-
-document.getElementById('file-input').addEventListener('change', readSingleFile, false);
-
-
-
 
 
 
