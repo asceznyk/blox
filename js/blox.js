13fda2569fa64c588d29b181cc66f5e83de18c49
asceznyk <asceznyk@gmail.com>
2021-09-27 22:03:12 +0530

    lexer done!

--- js/blox.js
+++ js/blox.js
@@ -1,6 +1,25 @@
+function removeComments(string) {
+  string = string.replace((/\#([\s\S]|[\r\n]).+?(?=\#)\#/gsi), "");
+  string = string.replace((/\`([\s\S]|[\r\n]).+?(?=\`)\`/gsi), "");
+  let first = string.search(/\S/);
+	if (first == -1) return "";
+	return string.slice(first);
+}
+
+function lexer(program) {
+	program = removeComments(program);
+	program = program.replace((/[\n]/g), " ");
+	program = program.replace((/[\s]/g), " ");
+	let tokens = program.match((/[A-Za-z]+|[0-9.0-9]+|[\=\+\-\*\/\@\$\%\(\)\{\}\,\<\>\[\]]/gmi))
+	
+	return tokens;
+}
+
+
 function run(program) {
-	shell.innerHTML = program;
-	//let parsed = parse(program)
+	let tokens = lexer(program);
+	console.log(tokens);
+	shell.innerHTML = '--running program--';
 }
 
 
