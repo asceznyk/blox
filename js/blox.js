5f92d0f6f7afcc63d934824ac9b2aff70eaaaba0
asceznyk <asceznyk@gmail.com>
2021-09-27 18:37:37 +0530

    final

--- js/blox.js
+++ js/blox.js
@@ -1,7 +1,16 @@
 function run(program) {
-	console.log(program);
+	shell.innerHTML = program;
 	//let parsed = parse(program)
 }
+
+
+let shell = document.getElementById("shell");
+let runBtn = document.getElementById("run");
+runBtn.addEventListener('click', function() {;
+	run(document.getElementById("code").value);
+});
+
+
 
 
 
