bf27fa35dbc84ba9c33074adbf451517562741d8
asceznyk <asceznyk@gmail.com>
2021-10-01 20:09:19 +0530

    ..

--- js/lexer.js
+++ js/lexer.js
@@ -1,5 +1,39 @@
-function lexer (program) {
+var NumLiteral = function(x) {
+	this.type = 'NumLiteral';
+	this.value = Number(x);
+}
 
+var Identifier = function(x) {
+	this.type = 'Identifier';
+	this.name = x;
+}
+
+function skipSpaceComments(string) {
+  string = string.replace((/\#([\s\S]|[\r\n]).+?(?=\#)\#/gsi), '');
+  string = string.replace((/\`([\s\S]|[\r\n]).+?(?=\`)\`/gsi), '');
+  let first = string.search(/\S/);
+	if (first == -1) return "";
+	return string.slice(first);
+}
+
+function classify(term) {
+
+}
+
+function lexer(program) {
+	program = skipSpaceComments(program);
+
+	let tokens = [];
+	while(true) {
+		let skip = program.search(/\s/g);
+		if(skip == -1) break
+		let term = program.slice(0, skip);
+		program = program.slice(skip+1);
+		console.log(term, '--', program);
+	}
+
+
+	return tokens;
 }
 
 
--- js/main.js
+++ js/main.js
@@ -1,43 +1,14 @@
 const Operators = ["=", "+", "-", "*", "/", "==", "<", ">"]; 
 
-function removeComments(string) {
-  string = string.replace((/\#([\s\S]|[\r\n]).+?(?=\#)\#/gsi), '');
-  string = string.replace((/\`([\s\S]|[\r\n]).+?(?=\`)\`/gsi), '');
-  let first = string.search(/\S/);
-	if (first == -1) return "";
-	return string.slice(first);
-}
-
 /*function lexer(program) {
 	program = removeComments(program);
 	//let tokens = program.match((/[A-Za-z]+|[0-9.0-9]+|[\=\+\-\*\/\@\:\!\$\%\(\)\{\}\,\<\>\[\]\n]|(["'])(?:(?=(\\?))\2.)*?\1/gmi));	
 	return program;
 }*/
 
-function checkStart(tokens) {	
-	if(Operators.includes(tokens[0])) {
-		throw new SyntaxError("Cannot start programs with Operators");
-	} 
-	return true
-}
-
-function parser(tokens, t, ast, block) {	
-	if(!checkStart(tokens)) return
-
-	block['name'] = tokens[t]
-	if (tokens[t+1] == '{') {
-		block['application'] = 'function'
-		block['body'] = parser(tokens, t+2, ast, {});
-	} else if (tokens[t+1] == '=') {
-		//block['name'] = tokens[t]
-	}
-	 
-	else if (tokens[t] == "\n") {
-		return parser(tokens, t+1, ast, block);
-	} 
-}
-
 function run(program) {
+	program += " ";
+	let tokens = lexer(program);
 	shell.innerHTML = '--running program--';
 }
 
