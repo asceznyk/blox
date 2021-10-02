3638bb1667e76c436da3ff042f1db28de442164d
asceznyk <asceznyk@gmail.com>
2021-10-02 14:47:01 +0530

    ..

--- js/lexer.js
+++ js/lexer.js
@@ -1,12 +1,45 @@
-var NumLiteral = function(x) {
+/*var NumLiteral = function(x) {
 	this.type = 'NumLiteral';
 	this.value = Number(x);
+}
+
+var StrLiteral = function(x) {
+	this.type = 'StrLiteral';
+	this.value = String(x);
+}
+
+var BoolLiteral = function(x) {
+	this.type = 'BoolLiteral';
+	this.value = Boolean(x);
 }
 
 var Identifier = function(x) {
 	this.type = 'Identifier';
 	this.name = x;
 }
+
+var Definition = function(x) {
+	this.type = 'Definition';
+	this.name = x;
+}
+
+var Operator = function(x) {
+	this.type = 'Operator';
+	this.name = x;
+}*/
+
+var Literal = function(ftype, x) {
+	this.value = ftype ==  Boolean ? JSON.parse(x) : ftype(x);
+	this.type = typeof this.value;
+}
+
+var Term = function(type, x) {
+	this.type = type;
+	this.name = x;
+}
+
+var Bools = ['true', 'false'];
+var Defs = ['def', 'while', 'for', 'in', 'if', 'else', 'return', 'print'];
 
 function skipSpaceComments(string) {
   string = string.replace((/\#([\s\S]|[\r\n]).+?(?=\#)\#/gsi), '');
@@ -16,23 +49,44 @@ function skipSpaceComments(string) {
 	return string.slice(first);
 }
 
-function classify(term) {
+function tokenize(program) {
+	let match;
+	let token;
+	
+	if(match = /^[A-Za-z]+/.exec(program)) {
+		token = match[0];
+		if(Defs.includes(token)) {
+			token = new Term('definition', token);
+		} else if(Bools.includes(token)) {
+			token = new Literal(Boolean, token);
+		} else {
+			token = new Term('identifier', token);	
+		}
+	} else if (match = /^(["'])(?:(?=(\\?))\2.)*?\1/.exec(program)) {
+		token = new Literal(String, match[0]);
+	} else if (match = /^[0-9.0-9]+/.exec(program)) {
+		token = new Literal(Number, match[0]);
+	} else if (match = /^[\=\+\-\*\/\@\:\!\$\%]/.exec(program)) {
+		token = new Term('operator', match[0]);
+	} else {
+		token = {}
+		match = [' '];
+	}
 
+	return [token, program.slice(match[0].length)];
 }
 
 function lexer(program) {
 	program = skipSpaceComments(program);
 
 	let tokens = [];
-	while(true) {
-		let skip = program.search(/\s/g);
-		if(skip == -1) break
-		let term = program.slice(0, skip);
-		program = program.slice(skip+1);
-		console.log(term, '--', program);
+	while(program.match(/\S/) != null) {
+		let [token, temp] = tokenize(program);
+		program = skipSpaceComments(temp);
+		tokens.push(token);
 	}
 
-
+	console.log(tokens);
 	return tokens;
 }
 
