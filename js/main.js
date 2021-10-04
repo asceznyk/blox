4aea6360b3d192a3561e609ea6d571dfb180404d
asceznyk <asceznyk@gmail.com>
2021-10-04 20:13:01 +0530

    ..

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-/*function lexer(program) {
-	program = removeComments(program);
-	//let tokens = program.match((/[A-Za-z]+|[0-9.0-9]+|[\=\+\-\*\/\@\:\!\$\%\(\)\{\}\,\<\>\[\]\n]|(["'])(?:(?=(\\?))\2.)*?\1/gmi));	
-	return program;
-}*/
-
 function run(program) {
 	program += " ";
 	let tokens = lexer(program);
