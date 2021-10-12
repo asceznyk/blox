1285a6eb75c3d2249066f25b69f1cac0777bbbeb
asceznyk <asceznyk@gmail.com>
2021-10-12 19:54:19 +0530

    added operator trees!

--- js/lexer.js
+++ js/lexer.js
@@ -1,16 +1,3 @@
-var Literal = function(ftype, x) {
-	this.value = ftype ==  Boolean ? JSON.parse(x) : ftype(x);
-	this.type = typeof this.value;
-}
-
-var Term = function(type, x) {
-	this.type = type;
-	this.name = x;
-}
-
-var Bools = ['true', 'false'];
-var Definitions = ['def', 'while', 'for', 'in', 'if', 'else', 'return', 'print'];
-var Operators = ['=', '+', '-', '*', '/', '==', '++', '--', '+=', '-='];
 //var Seperators = ['(', ')', '{', '}', '[', ']', ';', ':', ',', '.'];
 
 function skipSpaceComments(string) {
--- js/parser.js
+++ js/parser.js
@@ -1,14 +1,44 @@
 function parseOperators(tokens, t) {
 	let name = tokens[t-1];
 	let value = tokens[t+1];
-	let operation = tokens[t];
+	let branch = tokens[t];
 
-	operation['args'] = [name, value];
-	return operation;
+	branch['args'] = [name, value];
+	return branch;
+}
+
+function parsePrint(tokens, t, expr) {
+	let branch = tokens[t];
+	branch['expr'] = expr;
+	return branch;
+}
+
+function parseFunctions(tokens, t) {
+	let branch = tokens[t];
+
+	let sep = tokens[t+2]['name'];
+	if(sep !== '{' && sep !== '(') {
+		throw new SyntaxError('Expected { or (');
+	}
+
+	branch['id'] = tokens[t+1];
+	branch['body'] = [];
+	branch['args'] = [];
+
+	//add functionality for args and body
+	
+	return branch;
 }
 
 function parseDefs(tokens, t) {
+	let def = tokens[t];
+	let branch = {};
 
+	if(def['name'] == 'def') { 
+		branch = parseFunctions(tokens, t);
+	} else if (def['name'] == 'print') {
+		branch = parsePrint(tokens, t);
+	}
 }
 
 function parser(tokens, t = 0, ast = [{'type':'program', 'body':[]}]) {
