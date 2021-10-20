7c44d2f47d5d4fe56fe19f254c180423c49230f4
asceznyk <asceznyk@gmail.com>
2021-10-20 17:06:23 +0530

    ..

--- js/comments.js
+++
@@ -1,5 +0,0 @@
-/*function lexer(program) {
-	program = removeComments(program);
-	//let tokens = program.match((/[A-Za-z]+|[0-9.0-9]+|[\=\+\-\*\/\@\:\!\$\%\(\)\{\}\,\<\>\[\]\n]|(["'])(?:(?=(\\?))\2.)*?\1/gmi));	
-	return program;
-}*/
