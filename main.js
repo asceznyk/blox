ad5d3bf992f00e718b021667f7438cc30ec9aa6a
asceznyk <asceznyk@gmail.com>
2021-10-27 21:22:46 +0530

    ..

--- main.js
+++ main.js
@@ -23,8 +23,8 @@ clearBtn.addEventListener('click', function(){
 codeTxt.addEventListener('keydown', function(e) {
   if (e.key == 'Tab') {
     e.preventDefault();
-    var start = this.selectionStart;
-    var end = this.selectionEnd;
+    let start = this.selectionStart;
+    let end = this.selectionEnd;
 
     this.value = this.value.substring(0, start) + "\t" + this.value.substring(end);
     
